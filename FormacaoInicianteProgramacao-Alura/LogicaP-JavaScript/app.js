3c8a154e7d1ff485a41976d467a28726b2bebf1b
Fernando Silva <nando96sj@gmail.com>
2023-09-29 14:31:16 -0300

    Loops e Tentativas

--- FormacaoInicianteProgramacao-Alura/LogicaP-JavaScript/app.js
+++ FormacaoInicianteProgramacao-Alura/LogicaP-JavaScript/app.js
@@ -1,11 +1,22 @@
 alert('Boas Vindas ao Jogo do Número Secreto!');
 let numeroSecreto = 50;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 100');
+let chute;
+let tentativas = 1;
 
-// Se o chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else { 
-    alert('Você errou!')
+// enquanto chute não for igual ao número secreto..
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
+    // Se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else { 
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`); 
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
