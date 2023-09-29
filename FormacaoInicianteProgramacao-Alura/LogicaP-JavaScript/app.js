3701663c97b20001cff24688fd397665ecee0087
Fernando Silva <nando96sj@gmail.com>
2023-09-29 14:08:58 -0300

    Condicionais e Concatenação

--- FormacaoInicianteProgramacao-Alura/LogicaP-JavaScript/app.js
+++ FormacaoInicianteProgramacao-Alura/LogicaP-JavaScript/app.js
@@ -1,7 +1,11 @@
 alert('Boas Vindas ao Jogo do Número Secreto!');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um número entre 1 e 30');
+let numeroSecreto = 50;
+console.log(numeroSecreto);
+let chute = prompt('Escolha um número entre 1 e 100');
 
+// Se o chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto [5]');
-} 
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+} else { 
+    alert('Você errou!')
+}
