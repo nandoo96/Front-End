9bee8956de5e5bf30b4cb5c06ff22e4ec5b3aa41
Fernando Silva <nando96sj@gmail.com>
2023-09-29 11:22:48 -0300

    Iniciando com JavaScript

---
+++ FormacaoInicianteProgramacao-Alura/LogicaP-JavaScript/app.js
@@ -0,0 +1,7 @@
+alert('Boas Vindas ao Jogo do Número Secreto!');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (chute == numeroSecreto) {
+    console.log('Isso aí! Você descobriu o número secreto [5]');
+} 
