3bca5123862d5607c074ec404e9849c35b7213cd
Fernando Silva <nando96sj@gmail.com>
2023-09-30 15:37:37 -0300

    Funções

--- FormacaoInicianteProgramacao-Alura/LogicaProgramacao-FuncoesListas/app.js
+++ FormacaoInicianteProgramacao-Alura/LogicaProgramacao-FuncoesListas/app.js
@@ -1,9 +1,19 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+let numeroSecreto = gerarNumeroAleatorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jodo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um Número Entre 1 e 100');
 
 function verificarChute() {
-    console.log('O botão foi clicado')
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 100 + 1);
 }
